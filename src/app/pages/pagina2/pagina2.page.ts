ba89add9860f43811f66bbe37ad228546d1a8b50
katheryn-mena <katheryn.mena.fierro@gmail.com>
2021-09-25 10:45:53 -0300

    commit

--- src/app/pages/pagina2/pagina2.page.ts
+++ src/app/pages/pagina2/pagina2.page.ts
@@ -13,8 +13,44 @@ export class Pagina2Page implements OnInit {
     this.listar();
   }
 
-  listar() {
-    var datos = localStorage.getItem('datos');
+  lista_usuario = [
+    {
+      rut:"19921988-k",
+      nombre: "Katheryn",
+      edad: 22
+    },{
+      rut:"17616631-2",
+      nombre:"Vicente",
+      edad: 30
+    }];
+
+  actualizar(rut:String){
+    alert('Ha dado click en actualizar' + rut);
   }
 
+  eliminar(rut:String){
+    alert('Ha dado click en eliminar' + rut);
+  }
+
+  listar() {
+    var datos = localStorage.getItem('datos');
+    datos = datos.replace('[','');
+    datos = datos.replace(']','');
+    datos = datos.split('},{').join('};{');
+    var arreglo_temp = datos.split(';');
+    var usua;
+    var lista_temp = new Array();
+
+    for(let index = 0; index <arreglo_temp.length;index++){
+      var registro = arreglo_temp[index];
+      var usuarios = JSON.parse(registro);
+      usua={
+        rut: usuarios.rut, 
+        nombre: usuarios.nombre,
+        edad: usuarios.edad
+      };
+      lista_temp.push(usua);
+    }
+    this.lista_usuario = lista_temp;
+  }
 }
